9da6124c0632d3c443bb36ebd987b1d7b95de4eb
agueriv <rivasguerreroariel@gmail.com>
2024-02-11 21:04:38 +0100

    Alineamiento de paginación

---
+++ laravel/public/assets/js/main.js
@@ -0,0 +1,5 @@
+// Coger el ul de navigation para añadirle las clases necesarias
+$(document).ready(() => {
+    $('.pagination').closest('nav').addClass('m-0');
+    $('.pagination').closest('nav').addClass('ms-auto');
+});
