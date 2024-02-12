5fd611aa922298389000cfee176f15b5445491e3
agueriv <rivasguerreroariel@gmail.com>
2024-02-12 18:56:49 +0100

    Implementacion de nombre de usuario y botón de logout

--- laravel/resources/views/layout/app.blade.php
+++ laravel/resources/views/layout/app.blade.php
@@ -101,10 +101,10 @@
                             <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
                                 aria-label="Open user menu">
                                 <span class="avatar avatar-sm"
-                                    style="background-image: url(./static/avatars/000m.jpg)"></span>
+                                    style="background-image: url(./static/default_avatar.jpg)"></span>
                                 <div class="d-none d-xl-block ps-2">
-                                    <div>Carmelo</div>
-                                    <div class="mt-1 small text-muted">Superadmin</div>
+                                    <div>{{ Auth::user()->name }}</div>
+                                    <div class="mt-1 small text-muted">{{ Auth::user()->type }}</div>
                                 </div>
                             </a>
                             <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
