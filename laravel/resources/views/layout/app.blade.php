a26e38794d6b545372f0e3d808ca6a87ad49612a
agueriv <rivasguerreroariel@gmail.com>
2024-02-14 20:08:59 +0100

    Cambio de errorres de creación

--- laravel/resources/views/layout/app.blade.php
+++ laravel/resources/views/layout/app.blade.php
@@ -173,6 +173,7 @@
                 </div>
             @endif
             @if ($errors->any())
+            @error('message')
                 <div class="container-xl mt-5">
                     <div class="alert alert-danger alert-dismissible d-flex align-items-center" role="alert">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -181,12 +182,13 @@
                                 d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                         </svg>
                         <div style="margin-left: .8rem">
-                            {{ $errors->first() }}
+                            {{ $message }}
                         </div>
                         <button type="button" class="btn-close" data-bs-dismiss="alert"
                             aria-label="Close"></button>
                     </div>
                 </div>
+            @enderror
             @endif
             @yield('main-content')
             <footer class="footer footer-transparent d-print-none">
