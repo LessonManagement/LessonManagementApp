2f5bda8283c2edc997aa5e774aa385f18a5759f2
agueriv <rivasguerreroariel@gmail.com>
2024-02-15 08:47:55 +0100

    Creación de clave unica para relacion entre una formacion y un mismo módulo

--- laravel/database/migrations/2024_02_11_130142_create_modulo_formacion_table.php
+++ laravel/database/migrations/2024_02_11_130142_create_modulo_formacion_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,6 +17,9 @@ return new class extends Migration
             $table->foreignId('idformacion');
             $table->foreign('idformacion')->references('id')->on('formacion')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
+
+            // Restriccion para que un modulo no este asociado mas de una vez con la misma formacion
+            $table->unique(['idmodulo', 'idformacion']);
         });
     }
 
