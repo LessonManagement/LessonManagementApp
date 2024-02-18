b3e0f7adfe1ee58ffc4bfee5302753ee4ba2e7db
agueriv <rivasguerreroariel@gmail.com>
2024-02-19 00:12:45 +0100

    Creación de métodos y rutas de admin

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FormacionController;
 use App\Http\Controllers\GrupoController;
 use App\Http\Controllers\HomeController;
@@ -8,24 +9,13 @@ use App\Http\Controllers\ModuloController;
 use App\Http\Controllers\ProfesorController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-//Route::get('/', [HomeController::class, 'home'])->name('admin.home');
-
 // Rutas de recursos para gestionar las tablas
 Route::resource('profesor', ProfesorController::class);
 Route::resource('formacion', FormacionController::class);
 Route::resource('modulo', ModuloController::class);
 Route::resource('grupo', GrupoController::class);
 Route::resource('leccion', LeccionController::class);
+Route::resource('admin', AdminController::class);
 
 // 
 Auth::routes(['verify' => true]);
