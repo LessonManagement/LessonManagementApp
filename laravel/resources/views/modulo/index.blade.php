c426b35fcc325e8165dd48431a6b339979bb408e
agueriv <rivasguerreroariel@gmail.com>
2024-02-19 00:14:41 +0100

    Modificación del menú

--- laravel/resources/views/modulo/index.blade.php
+++ laravel/resources/views/modulo/index.blade.php
@@ -21,134 +21,150 @@
                 </span>
             </a>
         </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown" data-bs-auto-close="outside"
-                role="button" aria-expanded="false">
-                <span
-                    class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24"
-                        height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-                        <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                        <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                    </svg>
-                </span>
-                <span class="nav-link-title">
-                    Profesor
-                </span>
-            </a>
-            <div class="dropdown-menu">
-                <div class="dropdown-menu-columns">
-                    <div class="dropdown-menu-column">
-                        <a class="dropdown-item" href="{{ url('profesor') }}">
-                            Lista de profesores
-                        </a>
-                        <a class="dropdown-item" href="{{ url('profesor/create') }}">
-                            Añadir profesor
-                        </a>
+        @if (Auth::user()->email_verified_at != null || Auth::user()->type == 'admin')
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside" role="button" aria-expanded="false">
+                    <span
+                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24"
+                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                            <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                            <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+                        </svg>
+                    </span>
+                    <span class="nav-link-title">
+                        Profesor
+                    </span>
+                </a>
+                <div class="dropdown-menu">
+                    <div class="dropdown-menu-columns">
+                        <div class="dropdown-menu-column">
+                            <a class="dropdown-item" href="{{ url('profesor') }}">
+                                Lista de profesores
+                            </a>
+                            @if (Auth::user()->type != 'user')
+                                <a class="dropdown-item" href="{{ url('profesor/create') }}">
+                                    Añadir profesor
+                                </a>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown" data-bs-auto-close="outside"
-                role="button" aria-expanded="false">
-                <span
-                    class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group" width="24"
-                        height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                        <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
-                        <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                        <path d="M17 10h2a2 2 0 0 1 2 2v1" />
-                        <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                        <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
-                    </svg>
-                </span>
-                <span class="nav-link-title">
-                    Grupo
-                </span>
-            </a>
-            <div class="dropdown-menu">
-                <div class="dropdown-menu-columns">
-                    <div class="dropdown-menu-column">
-                        <a class="dropdown-item" href="{{ url('grupo') }}">
-                            Lista de grupos
-                        </a>
-                        <a class="dropdown-item" href="{{ url('grupo/create') }}">
-                            Añadir grupo
-                        </a>
+            </li>
+        @endif
+        @if (Auth::user()->email_verified_at != null || Auth::user()->type == 'admin')
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside" role="button" aria-expanded="false">
+                    <span
+                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                            <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
+                            <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                            <path d="M17 10h2a2 2 0 0 1 2 2v1" />
+                            <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                            <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
+                        </svg>
+                    </span>
+                    <span class="nav-link-title">
+                        Grupo
+                    </span>
+                </a>
+                <div class="dropdown-menu">
+                    <div class="dropdown-menu-columns">
+                        <div class="dropdown-menu-column">
+                            <a class="dropdown-item" href="{{ url('grupo') }}">
+                                Lista de grupos
+                            </a>
+                            @if (Auth::user()->type != 'user')
+                                <a class="dropdown-item" href="{{ url('grupo/create') }}">
+                                    Añadir grupo
+                                </a>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
-        <li class="nav-item dropdown active">
-            <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown" data-bs-auto-close="outside"
-                role="button" aria-expanded="false">
-                <span
-                    class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-stack-middle" width="24"
-                        height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M16 10l4 -2l-8 -4l-8 4l4 2" />
-                        <path d="M12 12l-4 -2l-4 2l8 4l8 -4l-4 -2l-4 2z" fill="currentColor" />
-                        <path d="M8 14l-4 2l8 4l8 -4l-4 -2" />
-                    </svg>
-                </span>
-                <span class="nav-link-title">
-                    Módulo
-                </span>
-            </a>
-            <div class="dropdown-menu">
-                <div class="dropdown-menu-columns">
-                    <div class="dropdown-menu-column">
-                        <a class="dropdown-item active" href="{{ url('modulo') }}">
-                            Lista de modulos
-                        </a>
-                        <a class="dropdown-item" href="{{ url('modulo/create') }}">
-                            Añadir modulo
-                        </a>
+            </li>
+        @endif
+        @if (Auth::user()->email_verified_at != null || Auth::user()->type == 'admin')
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside" role="button" aria-expanded="false">
+                    <span
+                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-stack-middle"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M16 10l4 -2l-8 -4l-8 4l4 2" />
+                            <path d="M12 12l-4 -2l-4 2l8 4l8 -4l-4 -2l-4 2z" fill="currentColor" />
+                            <path d="M8 14l-4 2l8 4l8 -4l-4 -2" />
+                        </svg>
+                    </span>
+                    <span class="nav-link-title">
+                        Módulo
+                    </span>
+                </a>
+                <div class="dropdown-menu">
+                    <div class="dropdown-menu-columns">
+                        <div class="dropdown-menu-column">
+                            <a class="dropdown-item active" href="{{ url('modulo') }}">
+                                Lista de modulos
+                            </a>
+                            @if (Auth::user()->type != 'user')
+                                <a class="dropdown-item" href="{{ url('modulo/create') }}">
+                                    Añadir modulo
+                                </a>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
-                data-bs-auto-close="outside" role="button" aria-expanded="false">
-                <span
-                    class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book-2" width="24"
-                        height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z" />
-                        <path d="M19 16h-12a2 2 0 0 0 -2 2" />
-                        <path d="M9 8h6" />
-                    </svg>
-                </span>
-                <span class="nav-link-title">
-                    Formación
-                </span>
-            </a>
-            <div class="dropdown-menu">
-                <div class="dropdown-menu-columns">
-                    <div class="dropdown-menu-column">
-                        <a class="dropdown-item" href="{{ url('formacion') }}">
-                            Lista de formaciones
-                        </a>
-                        <a class="dropdown-item" href="{{ url('formacion/create') }}">
-                            Añadir formación
-                        </a>
+            </li>
+        @endif
+        @if (Auth::user()->email_verified_at != null || Auth::user()->type == 'admin')
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside" role="button" aria-expanded="false">
+                    <span
+                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book-2"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z" />
+                            <path d="M19 16h-12a2 2 0 0 0 -2 2" />
+                            <path d="M9 8h6" />
+                        </svg>
+                    </span>
+                    <span class="nav-link-title">
+                        Formación
+                    </span>
+                </a>
+                <div class="dropdown-menu">
+                    <div class="dropdown-menu-columns">
+                        <div class="dropdown-menu-column">
+                            <a class="dropdown-item" href="{{ url('formacion') }}">
+                                Lista de formaciones
+                            </a>
+                            @if (Auth::user()->type != 'user')
+                                <a class="dropdown-item" href="{{ url('formacion/create') }}">
+                                    Añadir formación
+                                </a>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
+            </li>
+        @endif
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
                 data-bs-auto-close="outside" role="button" aria-expanded="false">
@@ -176,37 +192,38 @@
                 </div>
             </div>
         </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
-                data-bs-auto-close="outside" role="button" aria-expanded="false">
-                <span
-                    class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-shield"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                        fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M6 21v-2a4 4 0 0 1 4 -4h2" />
-                        <path d="M22 16c0 4 -2.5 6 -3.5 6s-3.5 -2 -3.5 -6c1 0 2.5 -.5 3.5 -1.5c1 1 2.5 1.5 3.5 1.5z" />
-                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                    </svg>
-                </span>
-                <span class="nav-link-title">
-                    Administrador
-                </span>
-            </a>
-            <div class="dropdown-menu">
-                <div class="dropdown-menu-columns">
-                    <div class="dropdown-menu-column">
-                        <a class="dropdown-item" href="{{ url('formacion') }}">
-                            Lista de administradores
-                        </a>
-                        <a class="dropdown-item" href="{{ url('formacion/create') }}">
-                            Añadir administrador
-                        </a>
+        @if (Auth::user()->type == 'root' || Auth::user()->type == 'Root')
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside" role="button" aria-expanded="false">
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-shield"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M6 21v-2a4 4 0 0 1 4 -4h2" />
+                            <path d="M22 16c0 4 -2.5 6 -3.5 6s-3.5 -2 -3.5 -6c1 0 2.5 -.5 3.5 -1.5c1 1 2.5 1.5 3.5 1.5z" />
+                            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                        </svg>
+                    </span>
+                    <span class="nav-link-title">
+                        Administrador
+                    </span>
+                </a>
+                <div class="dropdown-menu">
+                    <div class="dropdown-menu-columns">
+                        <div class="dropdown-menu-column">
+                            <a class="dropdown-item" href="{{ url('admin') }}">
+                                Lista de administradores
+                            </a>
+                            <a class="dropdown-item" href="{{ url('admin/create') }}">
+                                Añadir administrador
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
+            </li>
+        @endif
     </ul>
 @endsection
 
