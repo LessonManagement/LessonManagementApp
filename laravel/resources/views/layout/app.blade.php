8e186412299feda06a6d5fbd9e131dc50627847d
agueriv <rivasguerreroariel@gmail.com>
2024-02-18 14:00:39 +0100

    Cambio de menu bar

--- laravel/resources/views/layout/app.blade.php
+++ laravel/resources/views/layout/app.blade.php
@@ -47,7 +47,7 @@
                         </a>
                     </h1>
                     <div class="navbar-nav flex-row order-md-last">
-                        <div class="d-none d-md-flex">
+                        <div class="d-none d-md-flex me-2">
                             <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="Enable dark mode"
                                 data-bs-toggle="tooltip" data-bs-placement="bottom">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
@@ -71,31 +71,6 @@
                                         d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
                                 </svg>
                             </a>
-                            <div class="nav-item dropdown d-none d-md-flex me-3">
-                                <a href="#" class="nav-link px-0" data-bs-toggle="dropdown" tabindex="-1"
-                                    aria-label="Show notifications">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path
-                                            d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
-                                        <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-                                    </svg>
-                                    <span class="badge bg-red"></span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-end dropdown-menu-card">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h3 class="card-title">Last updates</h3>
-                                        </div>
-                                        <div class="list-group list-group-flush list-group-hoverable">
-                                            {{-- Last updates --}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
@@ -130,25 +105,6 @@
                     <div class="navbar">
                         <div class="container-xl">
                             @yield('menu')
-                            <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-                                <form action="./" method="get" autocomplete="off" novalidate>
-                                    <div class="input-icon">
-                                        <span class="input-icon-addon">
-                                            <!-- Download SVG icon from http://tabler-icons.io/i/search -->
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                                height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                stroke="currentColor" fill="none" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                                                <path d="M21 21l-6 -6" />
-                                            </svg>
-                                        </span>
-                                        <input type="text" value="" class="form-control"
-                                            placeholder="Search…" aria-label="Search in website">
-                                    </div>
-                                </form>
-                            </div>
                         </div>
                     </div>
                 </div>
