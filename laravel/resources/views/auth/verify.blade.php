c5ca4a3e71038c3515db3170572825d42ae59129
agueriv <rivasguerreroariel@gmail.com>
2024-02-19 17:50:16 +0100

    Modificación de plantilla para envio de email de verificación

--- laravel/resources/views/auth/verify.blade.php
+++ laravel/resources/views/auth/verify.blade.php
@@ -1,28 +1,35 @@
 @extends('layouts.app')
 
+@section('title', 'Email Verify - LessonManagement')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+    <div class="page page-center">
+        <div class="container container-tight py-4">
+            <div class="text-center mb-4">
+                <a href="." class="navbar-brand navbar-brand-autodark"><img
+                        src="{{ url('assets/static/lm_logo.svg') }}" height="70" alt=""></a>
+            </div>
+            <div class="text-center">
+                <div class="my-5">
+                    <h2 class="h1">Revisa tu email</h2>
+                    <p class="fs-h3 text-muted">
+                        Por favor, verifica tu dirección de email desde el enlace enviado a tu correo electrónico. Si no lo
+                        has recibido, <a id="resend-link" href="#" class="link">haz click aquí para solicitar otro.</a>
+                        <script>
+                            document.getElementById('resend-link').addEventListener('click', () => {
+                                document.getElementById('resend_form').submit();
+                            })
+                        </script>
+                    <form id="resend_form" class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
+                    </p>
+                </div>
+                <div class="text-center text-muted mt-3">
+                    ¿No ves el email? Revisa tu bandeja de spam!!<br />
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
