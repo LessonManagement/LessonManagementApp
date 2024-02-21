af230a5adbffedf29f13b21adb4f608422db1c80
agueriv <rivasguerreroariel@gmail.com>
2024-02-21 10:27:13 +0100

    Cambio de boton nuevo en index

--- laravel/resources/views/grupo/index.blade.php
+++ laravel/resources/views/grupo/index.blade.php
@@ -238,10 +238,24 @@
                 </ol>
             </div>
             <div class="row g-2 d-flex flex-row justify-content-between">
-                <h2 class="page-title col-xl-3">
-                    Lista de grupos
-                </h2>
-                <a class="index-create-btn col-xl-2" href="{{ url('grupo/create') }}">Añadir grupo</a>
+                <div class="col">
+                    <h2 class="page-title col-xl-3">
+                        Lista de grupos
+                    </h2>
+                </div>
+                <div class="col-auto ms-auto d-print-none">
+                    <a href="{{ url('grupo/create') }}" class="btn btn-primary">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 5l0 14" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                        Nuevo grupo
+                    </a>
+                </div>
             </div>
         </div>
     </div>
