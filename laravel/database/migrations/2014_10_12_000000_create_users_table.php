53168ddd7602edd4a428d916846ee0bca1e25544
agueriv <rivasguerreroariel@gmail.com>
2024-02-21 10:14:51 +0100

    Creación de usuario root por defecto al aplicar la migración

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -21,6 +22,11 @@ return new class extends Migration
             $table->enum('type', ['root', 'admin', 'user'])->default('user');
             $table->timestamps();
         });
+
+        // Creamos usuario root por defecto al aplicar las migraciones
+        $pass = Hash::make('lm2024');
+        $sql = "insert into users (name, email, email_verified_at, password, type, created_at, updated_at) VALUES ('Root', 'root@root.es', '2024-02-21 10:07:58', '". $pass ."', 'root', '2024-02-21 10:07:58', '2024-02-21 10:07:58')";
+        DB::statement($sql);
     }
 
     /**
