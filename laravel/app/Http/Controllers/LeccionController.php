d20ad10b8af7bfcbc9094aa7c81eb72517694889
agueriv <rivasguerreroariel@gmail.com>
2024-02-29 19:35:28 +0100

    Implementación de método index y create para generar las lecciones automáticamente

--- laravel/app/Http/Controllers/LeccionController.php
+++ laravel/app/Http/Controllers/LeccionController.php
@@ -5,15 +5,113 @@ namespace App\Http\Controllers;
 use App\Models\Grupo;
 use App\Models\Leccion;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LeccionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    const RPP = 10;
+    const ORDERBY = 'leccion.id';
+    const ORDERTYPE = 'asc';
+    const PARAMS = [
+        'rpp' => [
+            self::RPP => self::RPP,
+            5 => 0,
+            25 => 0,
+            50 => 0,
+            100 => 0
+        ],
+        'orderBy' => [
+            self::ORDERBY => self::ORDERBY,
+            'leccion.id' => 0,
+            'leccion.horas' => 0,
+            'modulo.denominacion' => 0,
+            'grupo.denominacion' => 0,
+        ],
+        'orderType' => [
+            self::ORDERTYPE => self::ORDERTYPE,
+            'desc' => 0
+        ]
+    ];
+    function __construct()
     {
-        return view('leccion.index');
+        // Para todas las rutas debes estar autenticado
+        $this->middleware('verificado')->except(['index']);
+        // Para todas las rutas que no sean el listado de lecciones, se debe estar verificado
+        // Para todas las rutas que no sean la index, se debe ser admin o root
+        $this->middleware('admin')->except(['index']);
+    }
+    public function index(Request $request)
+    {
+        $rpp = self::getFromRequest($request, 'rpp', self::RPP);
+        $orderBy = self::getFromRequest($request, 'orderBy', self::ORDERBY);
+        $orderType = self::getFromRequest($request, 'orderType', self::ORDERTYPE);
+        $q = $request->q;
+
+        $leccionQ = DB::table('leccion')
+            ->join('modulo', 'leccion.idmodulo', '=', 'modulo.id')
+            ->join('grupo', 'leccion.idgrupo', '=', 'grupo.id')
+            ->select(
+                'leccion.id AS id',
+                'modulo.denominacion AS modulo_denom',
+                'grupo.denominacion AS grupo_denom',
+                'leccion.horas AS horas',
+            );
+
+        if ($q != null) {
+            $leccionQ = $leccionQ->where('leccion.id', 'like', '%' . $q . '%')
+                ->orWhere('grupo.denominacion', 'like', '%' . $q . '%')
+                ->orWhere('modulo.denominacion', 'like', '%' . $q . '%')
+                ->orWhere('leccion.horas', 'like', '%' . $q . '%');
+        }
+
+        $lecciones = $leccionQ->orderBy($orderBy, $orderType)
+            ->orderBy(self::ORDERBY, self::ORDERTYPE)
+            ->paginate($rpp);
+
+        // Recuento total de modulos
+        $count_query = DB::select('select count(*) as leccion_count from leccion');
+        $leccion_count = $count_query[0]->leccion_count;
+
+        // Recuento de modulos mostrados
+        if ($lecciones->currentPage() === 1) {
+            $init_lecc = 1;
+            $last_lecc_page = $lecciones->perPage();
+        } else {
+            $last_lecc_page = $lecciones->currentPage() * $lecciones->perPage();
+            if ($leccion_count < $last_lecc_page) {
+                $last_lecc_page = $leccion_count;
+            }
+            $init_lecc = ($lecciones->currentPage() * $lecciones->perPage()) - $lecciones->perPage();
+        }
+
+        return view('leccion.index', [
+            'lecciones' => $lecciones,
+            'orderBy' => $orderBy,
+            'orderType' => $orderType,
+            'q' => $q,
+            'rpps' => self::getRpp(),
+            'rpp' => $rpp,
+            'leccion_count' => $leccion_count,
+            'init_lecc' => $init_lecc,
+            'last_lecc_page' => $last_lecc_page
+        ]);
+    }
+
+    private static function getRpp()
+    {
+        return [5 => 5, 10 => 10, 25 => 25, 50 => 50, 100 => 100];
+    }
+
+    private static function getFromRequest($request, $name, $defaultValue)
+    {
+        $value = array_key_first(self::PARAMS[$name]);
+        if ($request->$name != null) {
+            $value = $request->$name;
+        }
+        if (!isset(self::PARAMS[$name][$value])) {
+            $value = array_key_first(self::PARAMS[$name]);
+        }
+        return $value;
     }
 
     /**
@@ -21,15 +119,19 @@ class LeccionController extends Controller
      */
     public function create()
     {
-        // Recogemos todos los grupos
-        $grupos = Grupo::all();
-        // Recorremos la lista de grupos
-        foreach ($grupos as $grupo) {
-            foreach ($grupo->formacion->modulos as $mod) {
-                $this->newLeccion($grupo, $mod);
+        try {
+            // Recogemos todos los grupos
+            $grupos = Grupo::all();
+            // Recorremos la lista de grupos
+            foreach ($grupos as $grupo) {
+                foreach ($grupo->formacion->modulos as $mod) {
+                    $this->newLeccion($grupo, $mod);
+                }
             }
+            return redirect('leccion')->with(['message' => 'Lecciones generadas exitosamente.']);
+        } catch (\Exception $e) {
+            return back()->withInput()->withErrors(['message' => 'No se han podido generar las lecciones']);
         }
-        return view('leccion.create');
     }
 
     /**
@@ -81,7 +183,7 @@ class LeccionController extends Controller
         try {
             // Actualizamos el módulo
             $result = $leccion->update($request->all());
-            return redirect('modulo')->with(['message' => 'La lección se ha actualizado correctamente']);
+            return redirect('leccion')->with(['message' => 'La lección se ha actualizado correctamente']);
         } catch (\Exception $e) {
             return back()->withInput()->withErrors(['message' => 'La lección no se ha actualizado correctamente']);
         }
@@ -94,9 +196,9 @@ class LeccionController extends Controller
     {
         try {
             $leccion->delete();
-            return redirect('modulo')->with(['message' => 'La lección ha sido borrada correctamente']);
+            return redirect('leccion')->with(['message' => 'La lección ha sido borrada correctamente']);
         } catch (\Exception $e) {
-            return redirect('modulo')->withErrors(['message' => 'La lección no ha sido borrada']);
+            return back()->withInput()->withErrors(['message' => 'La lección no ha sido borrada']);
         }
     }
 }
