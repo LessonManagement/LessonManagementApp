4812f644d53beebf3e05c0f5096be6b6b0e63361
agueriv <rivasguerreroariel@gmail.com>
2024-03-01 00:53:56 +0100

    Cambios en datos por defecto en migración de profesores

--- laravel/database/migrations/2024_01_26_155747_create_profesors_table.php
+++ laravel/database/migrations/2024_01_26_155747_create_profesors_table.php
@@ -21,11 +21,8 @@ return new class extends Migration {
             $table->timestamps();
         });
 
-        $sql = "
-        insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('agueriv3110', 'Ariel', 'Guerrero', 'Rivas', 'agueriv3110@ieszaidinvergeles.org', 'desarrollo', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
-        insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('mjimpal1234', 'Mode', 'Martínez', 'Palenzuela', 'mode@ieszaidinvergeles.org', 'diseño', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
-        insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('rjumper', 'Raul', 'Jimenez', 'Perez', 'rauljimenez@ieszaidinvergeles.org', 'desarrollo', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
-        ";
+        // Damos de alta un 'profesor' auxiliar que usaremos para establacer las lecciones a null
+        $sql = "insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('lm2024', 'LessonManagement', 'SA', '2024', 'aux@lessonamnagement.com', 'auxiliar', '2024-02-21 10:07:58', '2024-02-21 10:07:58');";
         DB::statement($sql);
     }
 
