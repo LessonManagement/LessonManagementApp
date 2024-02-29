7491cc467fd012020991916cbc526398dca1314c
agueriv <rivasguerreroariel@gmail.com>
2024-03-01 00:54:45 +0100

    Implementación de listado con acciones sobre lecciones y control de permisos

--- laravel/resources/views/leccion/index.blade.php
+++ laravel/resources/views/leccion/index.blade.php
@@ -231,6 +231,7 @@
     @include('leccion.modals.deleteLeccion')
     @include('leccion.modals.generateLecciones')
     @include('leccion.modals.regenerateLecciones')
+    @include('leccion.modals.recargarLecciones')
     <div class="page-header d-print-none">
         <div class="container-xl">
             <div class="bread-crumbs mb-5">
@@ -248,14 +249,30 @@
                 @if (sizeof($lecciones) > 0 && (Auth::user()->type == 'root' || Auth::user()->type == 'admin'))
                     <div class="col-auto ms-auto d-print-none">
                         <a href="" class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#regenerateLeccionesModal">
+                            data-bs-target="#recargarLeccionesModal">
                             <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-refresh"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M12 5l0 14" />
-                                <path d="M5 12l14 0" />
+                                <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+                                <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
+                            </svg>
+                            Recargar lecciones
+                        </a>
+                    </div>
+                    <div class="col-auto ms-auto d-print-none">
+                        <a href="" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#regenerateLeccionesModal">
+                            <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-triangle"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 9v4" />
+                                <path
+                                    d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z" />
+                                <path d="M12 16h.01" />
                             </svg>
                             Regenerar lecciones
                         </a>
@@ -381,6 +398,35 @@
                                             </a>
                                         </th>
                                         <th>
+                                            Profesor
+                                            <a href="?rpp={{ $rpp }}&orderBy=profesor.nombre&orderType=asc&q={{ $q }}"
+                                                style="text-decoration: none">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-arrow-up" width="24"
+                                                    height="24" viewBox="0 0 24 24" stroke-width="1.5"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round" style="--tblr-icon-size: .9rem">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M12 5l0 14" />
+                                                    <path d="M18 11l-6 -6" />
+                                                    <path d="M6 11l6 -6" />
+                                                </svg>
+                                            </a>
+                                            <a
+                                                href="?rpp={{ $rpp }}&orderBy=profesor.nombre&orderType=desc&q={{ $q }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-arrow-down" width="24"
+                                                    height="24" viewBox="0 0 24 24" stroke-width="1.5"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round" style="--tblr-icon-size: .9rem">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M12 5l0 14" />
+                                                    <path d="M18 13l-6 6" />
+                                                    <path d="M6 13l6 6" />
+                                                </svg>
+                                            </a>
+                                        </th>
+                                        <th>
                                             Módulo
                                             <a href="?rpp={{ $rpp }}&orderBy=modulo.denominacion&orderType=asc&q={{ $q }}"
                                                 style="text-decoration: none">
@@ -449,6 +495,13 @@
                                             </td>
                                             <td>
                                                 {{ $leccion->grupo_denom }}
+                                            </td>
+                                            <td>
+                                                @if ($leccion->prof_nombre == 'LessonManagement')
+                                                    No asignado
+                                                @else
+                                                    {{ $leccion->prof_nombre }}
+                                                @endif
                                             </td>
                                             <td>
                                                 {{ $leccion->modulo_denom }}
