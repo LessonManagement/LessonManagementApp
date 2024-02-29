995ae4702f092d8dac0b9a266e8a0a6a3e170300
agueriv <rivasguerreroariel@gmail.com>
2024-02-29 11:17:12 +0100

    Creación de datos por defecto en migraciones

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Hash;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -32,7 +31,10 @@ return new class extends Migration
 
         // Creamos usuario root por defecto al aplicar las migraciones
         $pass = Hash::make('lm2024');
-        $sql = "insert into users (name, email, email_verified_at, password, type, created_at, updated_at) VALUES ('Root', 'root@root.es', '2024-02-21 10:07:58', '". $pass ."', 'root', '2024-02-21 10:07:58', '2024-02-21 10:07:58')";
+        $sql = "
+        insert into users (name, email, email_verified_at, password, type, created_at, updated_at) VALUES ('Root', 'root@root.es', '2024-02-21 10:07:58', '" . $pass . "', 'root', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
+        insert into users (name, email, email_verified_at, password, type, created_at, updated_at) VALUES ('Carmelo', 'dwes@ieszaidinvergeles.org', '2024-02-21 10:07:58', '" . $pass . "', 'root', '2024-02-21 10:07:58', '2024-02-21 10:07:58')
+        ";
         DB::statement($sql);
     }
 
--- laravel/database/migrations/2024_01_26_155747_create_profesors_table.php
+++ laravel/database/migrations/2024_01_26_155747_create_profesors_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,6 +20,13 @@ return new class extends Migration
             $table->string('especialidad', 100);
             $table->timestamps();
         });
+
+        $sql = "
+        insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('agueriv3110', 'Ariel', 'Guerrero', 'Rivas', 'agueriv3110@ieszaidinvergeles.org', 'desarrollo', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
+        insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('mjimpal1234', 'Mode', 'Martínez', 'Palenzuela', 'mode@ieszaidinvergeles.org', 'diseño', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
+        insert into users (seneca_username, nombre, apellido1, apellido2, email, especialidad, created_at, updated_at) VALUES ('rjumper', 'Raul', 'Jimenez', 'Perez', 'rauljimenez@ieszaidinvergeles.org', 'desarrollo', '2024-02-21 10:07:58', '2024-02-21 10:07:58');
+        ";
+        DB::statement($sql);
     }
 
     /**
