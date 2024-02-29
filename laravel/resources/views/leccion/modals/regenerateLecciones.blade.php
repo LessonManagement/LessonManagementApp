e4674087725b28481078baba9da8efb39c779f35
agueriv <rivasguerreroariel@gmail.com>
2024-03-01 00:55:08 +0100

    Modificación de modal para regenerar lecciones

--- laravel/resources/views/leccion/modals/regenerateLecciones.blade.php
+++ laravel/resources/views/leccion/modals/regenerateLecciones.blade.php
@@ -2,13 +2,22 @@
     <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
         <div class="modal-content">
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            <div class="modal-status bg-info"></div>
+            <div class="modal-status bg-warning"></div>
             <div class="modal-body text-center py-4">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-info icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 8v4" /><path d="M12 16h.01" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-warning icon-lg" width="24"
+                    height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 9v4" />
+                    <path
+                        d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z" />
+                    <path d="M12 16h.01" />
+                </svg>
                 <h3>¿Estás seguro?</h3>
-                <div class="text-muted">¿Estas seguro de que quieres regenerar las lecciones?</div>
+                <div class="text-muted">Esta acción sobreescribirá toda la base de datos de lecciones generando nuevas lecciones</div>
             </div>
             <form id="regenerateForm" action="{{ url('leccion/create') }}" method="get">
+                <input type="hidden" name="op" value="regen">
             </form>
             <div class="modal-footer">
                 <div class="w-100">
@@ -19,7 +28,7 @@
                             </a>
                         </div>
                         <div class="col">
-                            <button class="btn btn-info w-100" form="regenerateForm" type="submit">
+                            <button class="btn btn-warning w-100" form="regenerateForm" type="submit">
                                 Regenerar lecciones
                             </button>
                         </div>
